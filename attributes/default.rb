26ee097355c5d0dc176c6805603d868860fded8d
tonyjchong <tony.chong@kwarter.com>
2013-09-19 20:55:21 -0700

    fixed the filters and paths for s3 and the buffers

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default[:td_agent][:api_key] = ''
 default[:td_agent][:plugins] = ["udp" , "redshift"]
 default[:td_agent][:version] = "1.1.15-1"
 default[:td_agent][:s3endpoint] = "s3-us-west-2.amazonaws.com"
-default[:td_agent][:s3bucket] = "kwarter-redshift"
+default[:td_agent][:s3bucket] = "kwarter-redshift-us-west-2"
 
 
 default[:td_agent][:redshift][:host] = "kwarter.cokd89y6juoz.us-west-2.redshift.amazonaws.com"
