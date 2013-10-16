41586e3b8dade355b824eff46f4bb1f2faf7cdfa
tonyjchong <tony.chong@kwarter.com>
2013-10-16 14:35:13 -0700

    moved attributes to the role instead

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,14 @@
 default[:td_agent][:api_key] = ''
 
-default[:td_agent][:plugins] = ["udp" , "redshift"]
-default[:td_agent][:version] = "1.1.15-1"
-default[:td_agent][:s3endpoint] = "s3-us-west-2.amazonaws.com"
-default[:td_agent][:s3bucket] = "kwarter-redshift-us-west-2"
+default[:td_agent][:plugins] = nil
+default[:td_agent][:version] = nil
+default[:td_agent][:s3endpoint] = nil
+default[:td_agent][:s3bucket] = nil
 
 
-default[:td_agent][:redshift][:host] = "kwarter.cokd89y6juoz.us-west-2.redshift.amazonaws.com"
-default[:td_agent][:redshift][:port] = "5439"
-default[:td_agent][:redshift][:dbname] = "dev"
-default[:td_agent][:redshift][:user] = "platforms"
-default[:td_agent][:redshift][:password] = "Kwart3rMast3r"
-default[:td_agent][:redshift][:filetype] = "json"
+default[:td_agent][:redshift][:host] = nil
+default[:td_agent][:redshift][:port] = nil
+default[:td_agent][:redshift][:dbname] = nil
+default[:td_agent][:redshift][:user] = nil
+default[:td_agent][:redshift][:password] = nil
+default[:td_agent][:redshift][:filetype] = nil
