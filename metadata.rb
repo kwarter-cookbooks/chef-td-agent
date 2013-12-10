badf998c4b2dc823cd58e894a058d25f510dd873
Kirk Strauser <kirk.strauser@kwarter.com>
2013-12-10 14:00:35 -0800

    Add push_notifications tag

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "k@treasure-data.com"
 license          "All rights reserved"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.6"
+version          "0.0.8"
 recipe           "td-agent", "td-agent configuration"
 
 %w{redhat centos debian ubuntu}.each do |os|
